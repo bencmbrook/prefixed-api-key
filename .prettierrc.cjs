9b07fb63ed33e9c0b5d4ccc1e05b0f17b1f3e125
Ben Brook <bencmbrook@users.noreply.github.com>
2023-09-04 23:41:03 +0000

    Repo config

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+/** @type import('prettier').Config */
+const prettierConfig = {
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  singleQuote: true,
+};
+
+module.exports = prettierConfig;
