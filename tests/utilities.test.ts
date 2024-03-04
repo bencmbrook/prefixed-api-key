a9001afe231a95f78915eddb5d040940a424a902
bencmbrook <ben@transcend.io>
2024-03-04 17:36:43 -0500

    Add some tests for base58

--- tests/utilities.test.ts
+++ tests/utilities.test.ts
@@ -1,3 +1,4 @@
+import { randomBytes } from 'node:crypto';
 import { base58 } from '@scure/base';
 import test from 'ava';
 import {
@@ -16,11 +17,11 @@ const exampleKeyObject: APIKeyObject = {
   apiKey: 'mycompany_25RbpSyxVex_Gp6TRzhNvYA5GonfggbvNU',
 };
 
-const secret = extractSecret(exampleKeyObject.apiKey);
-const secretAsBuffer = extractSecretAsBuffer(exampleKeyObject.apiKey);
-
 test('hashSecret', async (t) => {
-  t.is(hashSecret(secretAsBuffer), exampleKeyObject.secretHash);
+  t.is(
+    hashSecret(extractSecretAsBuffer(exampleKeyObject.apiKey)),
+    exampleKeyObject.secretHash,
+  );
 });
 test('extractSecret', async (t) => {
   t.is(extractSecret(exampleKeyObject.apiKey), 'Gp6TRzhNvYA5GonfggbvNU');
@@ -36,4 +37,34 @@ test('checkAPIKey', async (t) => {
     await checkAPIKey(exampleKeyObject.apiKey, exampleKeyObject.secretHash),
     true,
   );
+});
+
+/**
+ * @scure/base is only used to encode and decode strings
+ *
+ * We trust it to
+ * - (A) Preserve integrity of key (and thus, its entropy)
+ * - (B) Not leak the key through some side effect
+ *
+ * This tests for (A)
+ */
+const inputFixture = new Uint8Array(
+  Buffer.from(
+    'aa0187becd7829b2d60cd8456dfb886af80b304804c17bc5b919c6b32e534736',
+    'hex',
+  ),
+);
+const encodedFixture = 'CSdfPvBnXbrc2RyiUZAFoAkiHaSt2Z56pV3txmTKtfzV';
+test('base58 encode - inputFixture encodes as expected', async (t) => {
+  t.is(base58.encode(inputFixture), encodedFixture);
+});
+test('base58 decode - encodedFixture decodes as expected', async (t) => {
+  t.deepEqual(base58.decode(encodedFixture), inputFixture);
+});
+test('base58 encode/decode', async (t) => {
+  const input = new Uint8Array(randomBytes(256));
+  const encoded = base58.encode(input);
+  t.assert(encoded.length > 340);
+  const output = base58.decode(encoded);
+  t.deepEqual(input, output);
 });
